b72e6aa81a859f40a825760ad875e799ba415949
arifwicakson0 <wicaksonoarif487@gmail.com>
2025-12-20 03:01:55 +0700

    menambah destinasientry

---
+++ app/src/main/java/com/example/tugas/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.tugas.uicontroller.route
+
+import com.example.tugas.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
