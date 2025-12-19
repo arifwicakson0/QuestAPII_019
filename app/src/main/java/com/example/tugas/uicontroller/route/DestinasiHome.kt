aca3ddc0385cf5c3bb8ec53f3b3195be1e1744db
arifwicakson0 <wicaksonoarif487@gmail.com>
2025-12-20 03:01:40 +0700

    menambah destinasihome

---
+++ app/src/main/java/com/example/tugas/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.tugas.uicontroller.route
+
+import com.example.tugas.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
